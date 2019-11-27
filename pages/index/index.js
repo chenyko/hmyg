5c2b7e7b839ebbcf4a057ae70643b053eedbb2e6
chenyko <55200520+chenyko@users.noreply.github.com>
2019-11-27 20:29:14 +0800

    首页楼层

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,4 +2,46 @@
 //获取应用实例
 const app = getApp()
 
-Page({})
+Page({
+  data: {
+    // 轮播图数组
+    swiperList: [],
+
+    //分类数组
+    navs: [],
+    // 楼层数组
+    floorList: [],
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (result) => {
+        // console.log(result);
+        this.setData({
+          swiperList: result.data.message
+        })
+      }
+    });
+
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: (result) => {
+        //  console.log(result);
+        this.setData({
+          navs: result.data.message
+        })
+
+      }
+    });
+
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+      success: (result) => {
+        // console.log(result.data.message);
+        this.setData({
+          floorList:result.data.message
+        })
+      }
+    })
+  }
+})
